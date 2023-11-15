0411a02e040a3dbb466de49fa69bff6728d69ee3
Yann Peclat <69076120+peclattdev@users.noreply.github.com>
2023-11-15 12:16:39 -0300

    script button to go top

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let btnMenu = document.getElementById("btn-menu")
 let menu = document.getElementById("menu-mobile")
 let overlay = document.getElementById("orvelay-menu-mobile")
+let btnTop = document.getElementById('btnTop')
+
 
 btnMenu.addEventListener("click", () => {
   menu.classList.add("abrir-menu")
@@ -13,3 +15,19 @@ menu.addEventListener("click", () => {
 overlay.addEventListener("click", () => {
   menu.classList.remove("abrir-menu")
 })
+
+btnTop.addEventListener('click', function () {
+  window.scrollTo(0,0);
+})
+
+document.addEventListener('scroll',ocultar)
+
+function ocultar() {
+  if(window.scrollY > 10) {
+    btnTop.style.display = 'flex'
+  } else {
+    btnTop.style.display = 'none'
+  };
+}
+
+ocultar();
